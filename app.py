0246207ee8db92d12f87dfb287a79440e26adfed
mitalletap <54956315+mitalletap@users.noreply.github.com>
2020-04-18 18:31:18 -0500

    JSON template

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask import jsonify
 
 app=Flask(__name__)
 
+operations = [{"name":"Who the fuck are you anyway","url":"/anyway/:company/:from","fields":[{"name":"Company","field":"company"},{"name":"From","field":"from"}]}]
+
+
+
+
 @app.route('/')
 def index():
     return jsonify('ADD SOMETHING')
@@ -14,3 +19,7 @@ def hello():
     post_id = request.args.get('id')
     return jsonify(post_id)
 
+
+@app.route('/operations')
+def operation():
+    return jsonify(operations)
