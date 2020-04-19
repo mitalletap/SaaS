d6e555fa2b514e453d17747366e4c37447e8d57a
kevin huynh <kevinhuynh269@yahoo.com>
2020-04-19 03:09:32 -0500

    added blueprints

--- controller/EndPointcontrollers.py
+++ controller/EndPointcontrollers.py
@@ -1,21 +1,24 @@
 from app import *
 # export FLASK_ENV=development
 
+#blueprint makes it all have a similar api route, may not seem like much but it will help out a ton in the future
+#especially if you have a large project
+mod = Blueprint('api', __name__,)
 input_file = open ("api_structure.json")
 operations = json.load(input_file)
 
-# @app.route('/')
-# def index():
-#     return jsonify('ADD SOMETHING')
+@app.route('/')
+def index():
+    return jsonify('I BROKE IT ')
 
 # All Shit
-@app.route('/operations')
+@mod.route('/operations')
 def allOperations():
     return jsonify(operations)     
 
 # Correct way to pass arguments through JSON Data and URL
 # What The Shit
-@app.route('/what-the-shit')
+@mod.route('/what-the-shit')
 def whatTheShit():
     post_response = operations[0]["name"].format(request.args.get("name"))
     new_operations = [post_response, operations[0]["data"]]
@@ -23,14 +26,14 @@ def whatTheShit():
 
 # What Is This Shit
 
-@app.route('/what-is-this-shit')
+@mod.route('/what-is-this-shit')
 def whatIsThisShit():
     post_response = operations[1]["name"]
     new_operations = [post_response, operations[1]["data"]]
     return jsonify(new_operations) 
     
 # How Do I Use This Shit
-@app.route('/how-do-i-use')
+@mod.route('/how-do-i-use')
 def howDoIUseThisShit():
     post_response = operations[2]["name"]
     new_operations = [post_response, operations[2]["data"]]
@@ -38,7 +41,7 @@ def howDoIUseThisShit():
 
 
 # Can You Believe This Shit
-@app.route('/can-you-believe')
+@mod.route('/can-you-believe')
 def canYouBelieve():
     post_response = operations[3]["name"]
     new_operations = [post_response, operations[3]["data"]]
@@ -46,42 +49,43 @@ def canYouBelieve():
 
 
 # How Do You Test This Shit
-@app.route('/how-do-you-test')
+@mod.route('/how-do-you-test')
 def howDoYouTest():
     post_response = operations[4]["name"]
     new_operations = [post_response, operations[4]["data"]]
     return jsonify(new_operations) 
 
 # Ive Got Shit To Do
-@app.route('/shit-to-do')
+@mod.route('/shit-to-do')
 def shitToDo():
     post_response = operations[5]["name"]
     new_operations = [post_response, operations[5]["data"]]
     return jsonify(new_operations) 
 
 # Same Shit Different 
-@app.route('/same-shit')
+@mod.route('/same-shit')
 def sameShit():
     post_response = operations[6]["name"]
     new_operations = [post_response, operations[6]["data"]]
     return jsonify(new_operations) 
 
 # Looks Like Shit
-@app.route('/look-like')
+@mod.route('/look-like')
 def looksLikeSHIT():
     post_response = operations[7]["name"]
     new_operations = [post_response, operations[7]["data"]]
     return jsonify(new_operations) 
 
-# Stop Talking Shit
-@app.route('/stop-talking')
+# Stop Talking Shit // also added the args
+@mod.route('/stop-talking')
 def stopTalking():
     post_response = operations[8]["name"]
-    new_operations = [post_response, operations[8]["data"]]
+    response = request.args.get('name')
+    new_operations = [post_response.format(response), operations[8]["data"]]
     return jsonify(new_operations) 
 
 # You Have Alot Of Shit
-@app.route('/alot-of')
+@mod.route('/alot-of')
 def alotOf():
     post_response = operations[8]["name"].format(request.args.get("name"))
     new_operations = [post_response, operations[8]["data"]]
@@ -90,11 +94,11 @@ def alotOf():
 
 
 
-
-
-
-@app.route('/operations')
+@mod.route('/operations')
 def operation():
     return jsonify(operations)
 
-
+#returns controller.Endpointcontrollers
+#@mod.route('/name')
+#def name():
+#    return jsonify(__name__)
