7913633353d46f630999981259b28c2a91a87c5e
kevin huynh <kevinhuynh269@yahoo.com>
2020-04-19 02:28:42 -0500

    added blueprints and learned what __name__ is

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import json
 
 app=Flask(__name__)
 from controller import EndPointcontrollers
+from controller.EndPointcontrollers import mod
+app.register_blueprint(
+    mod, url_prefix="/api")
+
 
 if __name__ == '__main__':
     app.run()
--- controller/EndPointcontrollers.py
+++ controller/EndPointcontrollers.py
@@ -1,19 +1,28 @@
 from app import *
-
+from flask import Blueprint
+#blueprint makes it all have a similar api route, may not seem like much but it will help out a ton in the future
+#especially if you have a large project
+mod = Blueprint('api', __name__,)
 input_file = open ("api_structure.json")
 operations = json.load(input_file)
 
-@app.route('/')
+
+@mod.route('/')
 def index():
     return jsonify('ADD SOMETHING')
 
 
-@app.route('/hello')
+@mod.route('/hello', methods=['POST','GET'])
 def hello():
-    post_id = request.args.get('id')
-    return jsonify(post_id)
+    post_name = request.args.get('name')
+    return jsonify(operations[0]["name"].format(post_name))
 
 
-@app.route('/operations')
+@mod.route('/operations')
 def operation():
     return jsonify(operations)
+    
+#module name
+@mod.route('/name')
+def name():
+    return __name__
