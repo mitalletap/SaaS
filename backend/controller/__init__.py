f9203645a53d92a79affe20c8ea09cf3738621d0
kevin huynh <kevinhuynh269@yahoo.com>
2020-05-26 18:06:53 -0500

    Hi Mital

--- backend/controller/__init__.py
+++ backend/controller/__init__.py
@@ -4,6 +4,4 @@ app = Flask(__name__)
 from controller import EndPointcontrollers
 import controller.EndPointcontrollers
 from controller.EndPointcontrollers import mod
-
-
 app.register_blueprint(mod,url_prefix="/api")
